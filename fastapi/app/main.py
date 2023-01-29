98262a3e51b509d1b519f490f94d4ad44fa1270f
ChanYoungHan <sanbon0812@naver.com>
2023-01-30 02:57:25 +0900

    Add database on fastapi

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -1,8 +1,11 @@
+from fastapi import FastAPI
 from typing import Union
 
-from fastapi import FastAPI
+from app.database.db_sqlalchemy import DB_SQLAlchemy, UserInfo, CreditCradInfo
+
 
 app = FastAPI()
+db = DB_SQLAlchemy()
 
 
 @app.get("/")
@@ -10,6 +13,14 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/get_user_credit_info/{name}")
+def read_item(name: str, q: Union[str, None] = None):
+    res_user_info: UserInfo = db.get_user_info(name=name)
+    res_credit_card_info: CreditCradInfo = db.get_credit_card_info(name=name)
+
+    return {
+        "user_name": name,
+        "user_id": res_user_info.id,
+        "credit_card_id": res_credit_card_info.id,
+        "credit_card_bank_name": res_credit_card_info.bank_name,
+    }
