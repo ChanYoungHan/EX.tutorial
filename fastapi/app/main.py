e3d8737486bca5ddfe61d9914a70e5a7d7f143ad
ChanYoungHan <sanbon0812@naver.com>
2023-02-09 09:08:24 +0900

    remove db on fastapi

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 from typing import Union
 
-from app.database.db_sqlalchemy import DB_SQLAlchemy, UserInfo, CreditCradInfo
+# from app.database.db_sqlalchemy import DB_SQLAlchemy, UserInfo, CreditCradInfo
 
 
 app = FastAPI()
-db = DB_SQLAlchemy()
+# db = DB_SQLAlchemy()
 
 
 @app.get("/")
@@ -13,14 +13,14 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/get_user_credit_info/{name}")
-def read_item(name: str, q: Union[str, None] = None):
-    res_user_info: UserInfo = db.get_user_info(name=name)
-    res_credit_card_info: CreditCradInfo = db.get_credit_card_info(name=name)
+# @app.get("/get_user_credit_info/{name}")
+# def read_item(name: str, q: Union[str, None] = None):
+#     res_user_info: UserInfo = db.get_user_info(name=name)
+#     res_credit_card_info: CreditCradInfo = db.get_credit_card_info(name=name)
 
-    return {
-        "user_name": name,
-        "user_id": res_user_info.id,
-        "credit_card_id": res_credit_card_info.id,
-        "credit_card_bank_name": res_credit_card_info.bank_name,
-    }
+#     return {
+#         "user_name": name,
+#         "user_id": res_user_info.id,
+#         "credit_card_id": res_credit_card_info.id,
+#         "credit_card_bank_name": res_credit_card_info.bank_name,
+#     }
